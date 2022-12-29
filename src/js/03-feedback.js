0c020e978832ee1a1fd60dae5604d1453c1b639f
Volodymyr Titarenko <xsligvaderx@gmail.com>
2022-12-28 20:59:21 -0600

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+storedData();
+
+form.addEventListener('input', throttle(onTextAreaInput, 500));
+form.addEventListener('submit', onformSubmit);
+
+const allFormData = {};
+
+function onTextAreaInput() {
+  const formData = new FormData(form);
+  formData.forEach((value, name) => (allFormData[name] = value));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(allFormData));
+}
+
+function onformSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function storedData() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    const savedFormData = JSON.parse(savedMessage);
+    const formElem = form.elements;
+
+    formElem['message'].value = savedFormData.message;
+    formElem['email'].value = savedFormData.email;
+  }
+}
